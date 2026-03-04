3fe26889663b3214eacb408e9b181aa8fc482df7
Sebastian <sebastianlakla@gmail.com>
2026-03-04 18:54:45 +0100

    Created directory with files vue extentions to create basic structure to write component

---
+++ src/components/product-cart/EmptyCart.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+    <slot name="empty-cart"/>
+</template>
+<style lang="scss" scoped></style>
---
+++ src/components/product-cart/ProductCartMain.vue
@@ -0,0 +1,9 @@
+<script setup>
+import EmptyCart from './EmptyCart.vue';
+import CartItem from './CartItem.vue';
+</script>
+<template>
+
+</template>
+<style lang="scss" scoped>
+</style>
