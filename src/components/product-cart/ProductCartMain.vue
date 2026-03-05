b998921ac82d4a313dbc51c746c28f21dc7d3c20
Sebastian <sebastianlakla@gmail.com>
2026-03-05 19:08:42 +0100

    Created styles for empty cart section

--- src/components/product-cart/ProductCartMain.vue
+++ src/components/product-cart/ProductCartMain.vue
@@ -15,18 +15,37 @@ const emptyIllustrationData = ref({
   <section class="cart-container">
     <EmptyCart v-if="cart.currentCartItem === 0">
       <template #empty-cart-content>
-        <p>Your Cart ({{ currentCartItem }})</p>
-        <img :src="emptyIllustrationData.src" :alt="emptyIllustrationData.alt" />
-        <p>Your added items will appear here</p>
+        <section class="empty-cart-content">
+          <h1 class="empty-cart-content__cart-amount">Your Cart ({{ currentCartItem }})</h1>
+          <img class="empty-cart-content__image" :src="emptyIllustrationData.src" :alt="emptyIllustrationData.alt" />
+          <p class="empty-cart-content__info">Your added items will appear here</p>
+        </section>
       </template>
     </EmptyCart>
     <CartItem v-else />
   </section>
 </template>
 <style lang="scss" scoped>
-@media (min-width: 992px) {
-  .cart-container {
-    padding-top: 2.75em;
+@use '../../assets/sass/colors.scss' as *;
+@use '../../assets/sass/fonts.scss' as *;
+@use '../../assets/sass/mixins.scss' as *;
+.cart-container {
+  padding-top: 2.75em;
+  .empty-cart-content{
+    &__cart-amount{
+      color: getColor('Red');
+      font-size: 1.5rem;
+    }
+    &__image{
+      width: 50%;
+      transform: translateX(50%);
+      padding-top: 1em;
+    }
+    &__info{
+      color: getColor('Rose-500');
+      font-weight: changeWeight('font-600');
+      text-align: center;
+    }
   }
 }
 </style>
