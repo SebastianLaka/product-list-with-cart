f8e0af01ad5673175fb71bc8b43075417df46b9b
Sebastian <sebastianlakla@gmail.com>
2026-03-17 18:34:54 +0100

    Created file for cart summary popup

---
+++ src/components/product-cart/CartSummaryPopup.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template></template>
+<style lang="scss" scoped>
+@use '../../assets/sass/colors.scss' as *;
+@use '../../assets/sass/fonts.scss' as *;
+@use '../../assets/sass/mixins.scss' as *;
+@use '../../assets/sass/breakpoints.scss' as *;
+</style>
